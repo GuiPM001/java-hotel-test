a424d833e96e1e8105fb596335e37f38ee4d3dcc
GuiPM001 <gui91905@gmail.com>
2021-01-31 21:37:43 -0300

    projeto finalizado

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente {
+
+    String tipoCliente;
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+    
+    
+    
+}
